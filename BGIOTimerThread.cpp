13a023c0dbf2665585f0a74631454e3d8920de9a
Jeon <sodtms@naver.com>
2018-10-17 11:03:50 +0900

    [1.02] BGIOTimer : BGIOTimerThread 객체 생성

---
+++ BGIOTimerThread.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "BGIOTimerThread.h"
+
+
+BGIOTimerThread::BGIOTimerThread()
+{
+}
+
+
+BGIOTimerThread::~BGIOTimerThread()
+{
+}
---
+++ BGIOTimerThread.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "BGIOThread.h"
+#include "BGIOTimer.h"
+#include "BGIOObject.h"
+
+class BGIOTimerThread
+{
+public:
+	BGIOTimerThread();
+	~BGIOTimerThread();
+};
+
