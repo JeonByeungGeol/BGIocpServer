f0a613ae24616ee826499fa9f547f09135a1a857
Jeon <sodtms@naver.com>
2018-10-17 12:11:25 +0900

    [1.02] BGIOTimer : BGIOTimerThread 객체 구현
    특징 :
    1. 하나의 스레드가 담당해서 동작
    2. 불필요한 while문을 돌지 않는다.
    (BG Proto 프로젝트에서는 항상 while을 돌면서 타이머 동작이 있는지 여부를 검사했다.)
    
    수정되어야 할 사항:
    1. 하나의 스레드에서 실제 객체의 동작까지 돌아간다.
    -> BG Proto 프로젝트에서는 타이머 스레드와 워커 스레드가 연동되어,
    실제 객체의 동작은 워커 스레드에서 담당하도록 처리하였다.

--- BGIOTimerThread.cpp
+++ BGIOTimerThread.cpp
@@ -1,12 +1,121 @@
 #include "stdafx.h"
 #include "BGIOTimerThread.h"
+#include "BGIOCompletionHandler.h"
 
+BGIOTimerThread::TimerQueue			BGIOTimerThread::s_timerQueue;
+// BGIOCriticalSection				BGIOTimerThread::s_lockTimer;
+ULONGLONG							BGIOTimerThread::s_n64TopTime;
+HANDLE								BGIOTimerThread::s_hTimer;
+BGIOTimerThread::BGIOTimerInstance	BGIOTimerThread::s_TimerInstance;
+BOOL								BGIOTimerThread::s_nStop;
 
-BGIOTimerThread::BGIOTimerThread()
+void BGIOTimerThread::BGIOTimerInstance::OnTimerCallback(int nId)
 {
+	// 타이머 큐가 비어있는 상태를 만들지 않기 위함
+	s_timerQueue.push( BGIOTimer( (BGIOObject*)this, GetTickCount64() + 24 * 3600 * 1000, nId) );
+	s_n64TopTime = s_timerQueue.top().m_n64Time;
 }
 
+BGIOTimerThread::BGIOTimerThread()
+	:m_pObject(nullptr)
+{
+	s_hTimer = CreateEvent(NULL, FALSE, FALSE, NULL);
+
+	m_vHandle[0] = s_hTimer;
+	m_vHandle[1] = CreateEvent(NULL, FALSE, FALSE, NULL);
+}
 
 BGIOTimerThread::~BGIOTimerThread()
 {
+	CloseHandle(m_vHandle[1]);
+}
+
+void BGIOTimerThread::Init()
+{
+	s_TimerInstance.OnTimerCallback();
+	s_TimerInstance.AddRef();
+
+	BGIOTimerThread* pTimerThread = new BGIOTimerThread();
+	pTimerThread->Begin(nullptr);
+}
+
+void BGIOTimerThread::AddTimer(BGIOObject* pObject, ULONGLONG n64Time, int nId)
+{
+	n64Time += GetTickCount64();
+
+	// s_lockTimer.Enter();
+	if (s_nStop) {
+		BG_LOG_WARNING("timer stoped");
+		// s_lockTimer.Leave();
+		return;
+	}
+
+	s_timerQueue.push( BGIOTimer(pObject, n64Time, nId) );
+
+	if ((LONG)(s_n64TopTime > n64Time)) {
+		s_n64TopTime = n64Time;
+		//s_lockTimer.Leave();
+		SetEvent(s_hTimer);
+	}
+	else {
+		//s_lockTimer.Leave();
+	}
+}
+
+void BGIOTimerThread::Run()
+{
+	for (; ;) {
+		ULONGLONG n64Tick = GetTickCount64();
+
+		//s_lockTimer.Enter();
+		LONG nWait = (LONG)(s_n64TopTime - n64Tick);
+		if (nWait <= 0) {
+			const BGIOTimer &top = s_timerQueue.top();
+			BGIOObject *pObject = top.m_pObject;
+			int id = top.m_nId;
+			for (; ;) {
+				s_timerQueue.pop();
+				pObject->OnTimerCallback(id);
+
+				const BGIOTimer &nextTop = s_timerQueue.top();
+				nWait = (LONG)(nextTop.m_n64Time - n64Tick);
+				if (nWait > 0) {
+					s_n64TopTime = nextTop.m_n64Time;
+					break;
+				}
+				pObject = nextTop.m_pObject;
+				id = nextTop.m_nId;
+			}
+		}
+		//s_lockTimer.Leave();
+
+		
+		
+
+
+		DWORD dwWaitResult = WaitForMultipleObjects(2, m_vHandle, FALSE, nWait);
+		if (dwWaitResult == WAIT_OBJECT_0) {
+			// 새로운 타이머 동작 들어옴
+			continue;
+		}
+		else if (dwWaitResult == (WAIT_OBJECT_0 + 1)) {
+			// 현재 시점 남은 타이머 동작 실행 후 종료
+			//s_lockTimer.Enter();
+			s_nStop = TRUE;
+			for (; ;) {
+				if (s_timerQueue.empty())
+					break;
+				BGIOObject *pObject = s_timerQueue.top().m_pObject;
+				pObject->Release(&pObject->m_nTimerRef);
+				s_timerQueue.pop;
+			}
+			//s_lockTimer.Leave();
+			break;
+		}
+	}
+}
+
+void BGIOTimerThread::OnTerminate()
+{
+	SetEvent(m_vHandle[1]);
 }
--- BGIOTimerThread.h
+++ BGIOTimerThread.h
@@ -4,10 +4,44 @@
 #include "BGIOTimer.h"
 #include "BGIOObject.h"
 
-class BGIOTimerThread
+class BGIOTimerThread : public BGIOThread
 {
+	using TimerVector = std::vector<BGIOTimer>;
+	using TimerQueue = std::priority_queue<BGIOTimer, TimerVector>;
+
+public:
+	class BGIOTimerInstance : public BGIOObject {
+	public:
+		virtual void OnTimerCallback(int id = 0);
+	};
+
+
+
+
+	/** static*/
+public:
+	static void Init();
+	static void AddTimer(BGIOObject* pObject, ULONGLONG n64Time, int nId);
+
+
+	static TimerQueue s_timerQueue;
+	// static BGIOCriticalSection s_lockTimer;
+	static ULONGLONG s_n64TopTime;
+	static HANDLE s_hTimer;
+	static BGIOTimerInstance s_TimerInstance;
+	static BOOL s_nStop;
+	
 public:
 	BGIOTimerThread();
-	~BGIOTimerThread();
+	virtual ~BGIOTimerThread();
+
+private:
+	virtual void Run();
+	virtual void OnTerminate();
+
+
+protected:
+	HANDLE m_vHandle[2];
+	BGIOObject* m_pObject;
 };
 
