0d1f6188642097c17f9a141499b6502cde7e79be
Jeon <sodtms@naver.com>
2018-11-05 11:29:05 +0900

    [1.03] BGIOLock : BGRWLock, BGIOSpinLock,  BGIOCriticalSection, BGScopedLock 구현 완료
     해당 기능들은 interlock함수와 window 기능을 사용해서 구현했다.
     하지만 c++11, c++14, c++17에서 존재하는 표준 기능들로 대체할지 고민

--- BGLock.cpp
+++ BGLock.cpp
@@ -2,6 +2,12 @@
 #include "BGLock.h"
 
 
+#include <shared_mutex>
+void test()
+{
+	std::shared_lock<std::shared_mutex> lock;
+}
+
 //------------------------------- ABCDE ==================================//
 //----------------------------------------------------------------------
 thread_local BGLockInfo tlsLockInfo; //C++11 표준 TLS 변수
@@ -22,8 +28,6 @@ BGLockInfo::~BGLockInfo()
 	BGILock::s_rwLock.WriteUnlock();
 }
 //----------------------------------------------------------------------
-
-
 
 
 //------------------------------- BGILock ==================================//
@@ -58,6 +62,7 @@ BGILock::BGILock()
 
 	s_rwLock.WriteUnlock();
 }
+
 void BGILock::IncPendingLock()
 {
 	InterlockedExchange(&tlsLockInfo.m_n64LastLockedTicks, ::GetTickCount64()); // //먼저 해줘야 오랜만에 걸린 락에 대한 오류가 안생김
@@ -69,6 +74,71 @@ void BGILock::DecPendingLock()
 	InterlockedDecrement(&tlsLockInfo.m_dwPendingLock);
 }
 //----------------------------------------------------------------------
+
+
+
+
+//------------------------------- BGIOSpinLock ==================================//
+BGIOSpinLock::BGIOSpinLock()
+	: m_nLock(0)
+{
+}
+
+void BGIOSpinLock::Enter()
+{
+	IncPendingLock();
+
+	if (InterlockedExchange(&m_nLock, 1))
+		Wait();
+}
+
+void BGIOSpinLock::Leave()
+{
+	DecPendingLock();
+
+	InterlockedExchange(&m_nLock, 0);
+}
+
+BOOL BGIOSpinLock::TryEnter()
+{
+	return InterlockedExchange(&m_nLock, 1) == 0;
+}
+
+
+void BGIOSpinLock::Wait()
+{
+	// _mm_pause()
+	// 멀티쓰레드 프로그래밍에서 락을 잡는 타이밍이 짧다면 이론족으로 spin lock을 사용하여 busy waiting을 하는 것이 성능에 좋다고 알려져 있다.
+	// 하지만 실제로 spin lock으로 효과를 보기 쉽지 않은데, CPU에 이를 위해 pause라는 명령어가 존재한다.
+	// spin lock이라는게 결국 변수의 값이 원하는 값이 될때까지 계속 loop를 돌리는 것인데,
+	// puase라는 명령어를 통해 CPU에게 지금 spin loop를 돌고 있다는 힌트를 주고,
+	// CPU 가 메모리버스 속도 등을 감안해서 변수가 바뀌는데 필요한 시간만큼 다음 명령어를 실행하지 않는 것이다.
+	// https://software.intel.com/sites/default/files/m/d/4/1/d/8/17689_w_spinlock.pdf (w_spinlock.pdf)
+
+	int count = 4000;
+	while (--count >= 0) {
+		if (InterlockedExchange(&m_nLock, 1) == 0)
+			return;
+		_mm_pause();
+	}
+
+	count = 4000;
+	while (--count >= 0) {
+		SwitchToThread();
+		if (InterlockedExchange(&m_nLock, 1) == 0)
+			return;
+	}
+
+	for (; ;) {
+		Sleep(1000);
+		if (InterlockedExchange(&m_nLock, 1) == 0)
+			return;
+	}
+}
+
+//----------------------------------------------------------------------
+
+
 
 
 //------------------------------- BGRWLock ==================================//
@@ -96,11 +166,9 @@ void BGRWLock::Wait()
 	// CPU 가 메모리버스 속도 등을 감안해서 변수가 바뀌는데 필요한 시간만큼 다음 명령어를 실행하지 않는 것이다.
 	// https://software.intel.com/sites/default/files/m/d/4/1/d/8/17689_w_spinlock.pdf (w_spinlock.pdf)
 
-	int count{ 0 };
-
-	count = 4000;
+	 int count = 4000;
 	while (--count >= 0) {
-		if(InterlockedExchange(&m_nLock, 1) == 0)
+		if (InterlockedExchange(&m_nLock, 1) == 0)
 			return;
 		_mm_pause();
 	}
@@ -132,26 +200,130 @@ void BGRWLock::Leave()
 
 void BGRWLock::ReadLock()
 {
+	IncPendingLock();
+
+	int nCount;
+
+	nCount = InterlockedIncrement(&m_nCount);
+	if (nCount & 0xff00)
+	{
+		Enter();
+		nCount = m_nCount;
+		do
+		{
+
+		} while ((nCount & 0xff00) != 0);
+
+		if (nCount & 0xff0000)
+		{
+			Leave();
+			SetEvent(m_hREvent);
+		}
+		else
+			Leave();
+	}
 
 }
 
 void BGRWLock::ReadUnlock()
 {
+	DecPendingLock();
 
+	int nCount = InterlockedDecrement(&m_nCount);
+	//if ( ( nCount & 0x8080 ) != 0 )
+	//	LOG_ERR( L"CRWLock::ReadUnlock() count error" );
+	if ((short)nCount == 0 && (nCount & 0xff000000))
+	{
+		SetEvent(m_hREvent);
+	}
 }
 
 void BGRWLock::WriteLock()
 {
+	IncPendingLock();
 
+	int nCount{ 0 };
+	nCount = InterlockedExchangeAdd(&m_nCount, 0x100);
+	if ((short)nCount)
+	{
+		Enter();
+		do
+		{
+			nCount = InterlockedExchangeAdd(&m_nCount, 0x1000000 - 0x100) + 0x1000000 - 0x100;
+			if ((short)nCount == 0)
+			{
+				Leave();
+				SetEvent(m_hWEvent);
+			}
+			else
+				Leave();
+
+			WaitForSingleObject(m_hWEvent, INFINITE);
+			Enter();
+			nCount = InterlockedExchangeAdd(&m_nCount, -0x1000000 + 0x100);
+		}
+		while ((short)nCount);
+		Leave();
+	}
 }
 
 void BGRWLock::WriteUnlock()
 {
+	DecPendingLock();
 
+
+	int	nCount = InterlockedExchangeAdd(&m_nCount, -0x100) - 0x100;
+	//if ( ( nCount & 0x8080 ) != 0 )
+	//	LOG_ERR( L"CRWLock::WriteUnlock() count error" );
+	if ((short)nCount == 0 && (nCount & 0xff000000))
+	{
+		SetEvent(m_hWEvent);
+	}
 }
 
 BOOL BGRWLock::WriteTryLock()
 {
+	if ((short)InterlockedExchangeAdd(&m_nCount, 0x100))
+	{
+		InterlockedExchangeAdd(&m_nCount, -0x100);
+		return FALSE;
+	}
+
 	return TRUE;
 }
+//----------------------------------------------------------------------
+
+
+
+//------------------------------- BGScopedLock ==================================//
+BGScopedLock::BGScopedLock(BGILock& lock)
+	: m_Lock(lock)
+	, m_bLeaved(FALSE)
+{
+	m_Lock.Enter();
+}
+
+BGScopedLock::~BGScopedLock()
+{
+	if (m_bLeaved == FALSE)
+		m_Lock.Leave();
+}
+
+void BGScopedLock::ForceLeave()
+{
+	if (m_bLeaved == TRUE)
+		return;
+
+	if (m_bLeaved == FALSE)
+	{
+		m_Lock.Leave();
+		m_bLeaved = TRUE;
+	}
+}
+//----------------------------------------------------------------------
+
+
+
+
+//------------------------------- BGRWLock ==================================//
 //----------------------------------------------------------------------
--- BGLock.h
+++ BGLock.h
@@ -4,9 +4,9 @@ class BGILock;
 class BGRWLock;
 
 /** 
- * 스레드 로컬 변수로 선언된다.
- * 스레드마다 관리하는 객체로 스레드 소멸시에 삭제된다.
-*/
+  * 스레드 로컬 변수로 선언된다.
+  * 스레드마다 관리하는 객체로 스레드 소멸시에 삭제된다.
+  */
 class BGLockInfo
 {
 public:
@@ -21,10 +21,10 @@ public:
 };
 
 /**
- * 가장 상위의 BGILock 객체
- * static 변수로 LockInfo객체를 map으로 관리한다.
- * 해당 map을 동기화 시키기 위한 rwLock을 가지고 있다.
-*/
+  * 가장 상위의 BGILock 객체
+  * static 변수로 LockInfo객체를 map으로 관리한다.
+  * 해당 map을 동기화 시키기 위한 rwLock을 가지고 있다.
+  */
 class BGILock
 {
 public:
@@ -43,6 +43,54 @@ public:
 	void DecPendingLock();
 };
 
+/**
+  * IOSpin Lock 객체
+  *
+  */
+class BGIOSpinLock : public BGILock
+{
+public:
+	BGIOSpinLock();
+
+	virtual void Enter();
+	virtual void Leave();
+	BOOL TryEnter();
+
+protected:
+	void Wait();
+
+	/** lock 여부 (1: lock, 0: unlock)*/
+	long m_nLock;
+};
+
+/**
+  * IOCriticalSection 객체
+  *
+  */
+class BGIOCriticalSection : public BGILock
+{
+public:
+	CRITICAL_SECTION m_critical_section;
+
+public:
+	BGIOCriticalSection() { InitializeCriticalSection(&m_critical_section); }
+	BGIOCriticalSection(DWORD dwSpinCount) { (void)InitializeCriticalSectionAndSpinCount(&m_critical_section, dwSpinCount); }
+	~BGIOCriticalSection() { DeleteCriticalSection(&m_critical_section); }
+
+	_Acquires_lock_(m_critical_section)
+		void	Enter() { IncPendingLock(); EnterCriticalSection(&m_critical_section); }
+
+	_Releases_lock_(m_critical_section)
+		void	Leave() { DecPendingLock(); LeaveCriticalSection(&m_critical_section); }
+
+	BOOL	TryEnter() { return TryEnterCriticalSection(&m_critical_section); }
+};
+
+
+/**
+  * RW Lock 객체
+  * Read, Wtite 동기화 수준을 다르게 할 수 있다.
+  */
 class BGRWLock : public BGILock
 {
 public:
@@ -56,7 +104,10 @@ public:
 	BOOL WriteTryLock();
 
 protected:
+	/**  */
 	long m_nCount;
+
+	/** 1 또는 0 유지 */
 	long m_nLock;
 	HANDLE m_hREvent;
 	HANDLE m_hWEvent;
@@ -66,3 +117,23 @@ protected:
 	virtual void Leave();
 };
 
+
+/**
+  * Scoped Lock 객체
+  
+  */
+struct BGScopedLock
+{
+public:
+	explicit BGScopedLock(BGILock& lock);
+	~BGScopedLock();
+
+	void ForceLeave();
+	
+private:
+	/** m_Lock가 locking중인지 여부*/
+	BOOL m_bLeaved;
+
+	/** Scoped에서 사용할 lock 객체*/
+	BGILock& m_Lock;
+};
