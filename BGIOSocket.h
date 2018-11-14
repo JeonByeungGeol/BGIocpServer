5de0ca3d78f73cb5e1a0563f755393a1a91e08c3
Jeon <sodtms@naver.com>
2018-11-14 17:52:23 +0900

    [1.05] BGIOWorkerThread : 주석 추가

--- BGIOSocket.h
+++ BGIOSocket.h
@@ -6,6 +6,23 @@
 
 class BGIOCompletionHandler;
 
+/**
+ * [BGIOSocket 객체]
+ * 
+ * 초기화
+ * Initialize()		->	OnRead()
+ *
+ * IO동작
+ * OnIOCallback()	->	ReadCallback()		-> OnRead()		->	Process() 
+ *					->	WriteCallback()		-> OnWrite()
+ *					->	OnTimer()
+ *
+ * 
+ * BGIOSocket 객체를 상속후에 OnRead()를 구현
+ * OnRead에서 패킷 처리할 Process를 가진 객체를 만든 후에
+ * IOCP객체를 통해 호출해 준다.
+ */
+
 class BGIOSocket : public BGIOObject
 {
 public:
@@ -28,11 +45,21 @@ public:
 	long		PendingWrite();
 
 protected:
-	virtual	void		OnIOCallback(BOOL bSuccess, DWORD dwTransferred, LPOVERLAPPED lpOverlapped);
-	void				ReadCallback(DWORD dwTransferred);
-	void				WriteCallback(DWORD dwTransferred);
+	/** */
 	virtual	void		OnClose() = 0;
+
+	/** IOCP Handler를 통해 호출되는 함수*/
+	virtual	void		OnIOCallback(BOOL bSuccess, DWORD dwTransferred, LPOVERLAPPED lpOverlapped);
+
+	/** WSARecv 호출 이후 OnIOCallback에서 호출되는 함수*/
+	void				ReadCallback(DWORD dwTransferred);
+
+	/** WSASend 호출 이후 OnIOCallback에서 호출되는 함수*/
+	void				WriteCallback(DWORD dwTransferred);
+
+	/** ReadCallback 에서 호출*/
 	virtual	void		OnRead() = 0;
+	
 
 
 public:
@@ -40,8 +67,14 @@ public:
 
 protected:
 	BGIOCriticalSection			m_lock;
+
+	/** WSARecv에서 사용하는 버퍼*/
 	OVERLAPPED					m_overlappedRead;
+
+	/** WSASend에서 사용하는 버퍼*/
 	OVERLAPPED					m_overlappedWrite;
+
+	/***/
 	BGIOBuffer*					m_pReadBuf;
 	BGIOBuffer*					m_pFirstBuf;
 	BGIOBuffer*					m_pLastBuf;
