4826842a962f4f4425e70f074a70821caf115fcc
Jeon <sodtms@naver.com>
2018-12-07 11:55:53 +0900

    [1.07] 테스트 환경 구축 : BGIOServer을 상속받는 샘플 TestServer 객체 구현

---
+++ BGTestServer.cpp
@@ -0,0 +1,106 @@
+#include "stdafx.h"
+#include "BGTestServer.h"
+
+#include "BGMainConfig.h"
+#include "BGLock.h"
+#include "BGLayer.h"
+#include "BGIOServer.h"
+
+#include "BGTestSocket.h"
+
+long							BGTestServer::s_nServerBit;
+BGTestServer::SocketMap			BGTestServer::s_mapSocket;
+
+static BGTestServer				g_server;
+static BGRWLock					g_lock;
+static int						g_nCount;
+
+void BGTestServer::Start(int nLayerId)
+{
+	int nResult = g_server.BGIOServer::Start( BGLayer::GetIOCP(nLayerId), BGMainConfig::s_nPort);
+	if (nResult == false) {
+		BG_LOG_ERROR("BGIOServer Start Fail | nLayerId:%d, s_nPort:%d", nLayerId, BGMainConfig::s_nPort);
+	}
+}
+
+BGIOSocket* BGTestServer::CreateSocket(SOCKET newSocket, sockaddr_in* addr)
+{
+	BGTestSocket* pSocket = new BGTestSocket(newSocket, addr);
+	return pSocket;
+}
+
+
+void BGTestServer::Add(BGTestSocket* pSocket)
+{
+	g_lock.WriteLock();
+
+	s_mapSocket.insert(SocketMap::value_type(pSocket->Id(), pSocket));
+	g_nCount++;
+
+	g_lock.WriteUnlock();
+}
+
+void BGTestServer::Remove(BGTestSocket* pSocket)
+{
+	g_lock.WriteLock();
+
+	g_nCount--;
+	s_mapSocket.erase(pSocket->Id());
+
+	g_lock.WriteUnlock();
+}
+
+void BGTestServer::Stop()
+{
+	g_server.BGIOServer::Stop();
+
+	g_lock.ReadLock();
+	
+	// pSocket->CloseSocket();
+
+	g_lock.ReadUnlock();
+}
+
+int BGTestServer::Size()
+{
+	return g_nCount;
+}
+
+BGTestSocket* BGTestServer::FindSocket(long nId)
+{
+	BGTestSocket* pSocket{ nullptr };
+
+	g_lock.ReadLock();
+	
+	SocketMap::iterator it = s_mapSocket.find(nId);
+	if (it != s_mapSocket.end())
+	{
+		pSocket = it->second;
+		pSocket->AddRef();
+	}
+
+	g_lock.ReadUnlock();
+	return pSocket;
+}
+
+BGTestPlayer* BGTestServer::FindPlayer(long nId)
+{
+	BGTestPlayer* pPlayer{ nullptr };
+
+	g_lock.ReadLock();
+
+	SocketMap::iterator it = s_mapSocket.find(nId);
+	if (it != s_mapSocket.end())
+	{
+		BGTestSocket* pSocket = it->second;
+		pSocket->Lock();
+		pPlayer = pSocket->m_pPlayer;
+		if (pPlayer)
+		{
+			//pPlayer->AddRef(&pPlayer->m_nFindRef);
+		}
+		pSocket->Unlock();		
+	}
+	g_lock.ReadUnlock();
+	return pPlayer;
+}
---
+++ BGTestServer.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "BGIOServer.h"
+
+class BGTestSocket;
+class BGTestPlayer;
+
+class BGTestServer : public BGIOServer
+{
+public:
+	using SocketMap = std::map<int, BGTestSocket*>;
+
+	static	long		s_nServerBit;
+	static	SocketMap	s_mapSocket;
+
+
+
+public:
+	static	void				BitSet(int nBit) {}
+	static	void				BitReset(int nBit) {}
+	static	bool				BitIs(int nBit) {}
+	static	bool				ServerOnIs() {}
+	static	bool				ServerStartIs() {}
+		
+	/** iocp 객체와 port를 얻어, BGIOServer::Start를 호출합니다. */
+	static	void				Start(int nLayerId);
+
+	/** Accept신호가 들어온 이후에 BGIOServer::OnIOCallback 에서 호출됩니다. */
+	virtual	BGIOSocket*			CreateSocket(SOCKET newSocket, sockaddr_in* addr);
+
+	/** 소켓을 맵에 추가합니다.*/
+	static	void				Add(BGTestSocket* pSocket);
+
+	/** 소켓을 맵에서 제거합니다. */
+	static	void				Remove(BGTestSocket* pSocket);
+
+	/** BGIOServer::Stop을 호출하고, 모든 소켓에 대해 CloseSocket을 호출합니다.*/
+	static	void				Stop();
+
+	/** 등록된 소켓의 갯수를 확인합니다.*/
+	static	int					Size();
+
+	/** 소켓 검색 */
+	static	BGTestSocket*		FindSocket(long nId);
+
+	/** 소켓을 찾은 이후에, Player를 검색 */
+	static	BGTestPlayer*		FindPlayer(long nId);
+
+};
+
