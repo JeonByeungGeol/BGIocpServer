271800e36c92255fc1b18721e9770b95cf656349
Jeon <sodtms@naver.com>
2018-12-07 15:49:44 +0900

    [1.07] 테스트 환경 구축 : 서버 종료 로직 확인 코드

--- BGIocpServer.cpp
+++ BGIocpServer.cpp
@@ -34,6 +34,7 @@ int main()
 		return -1;
 	}
 	
+	BG_LOG_INFO("SERVER ON STARTED");
 	BGTestSocket::s_nNetworkId = BGLayer::Add(BGMainConfig::s_nNumberOfThreads);
 	BGTestSocket::s_pIOCPHandler = BGLayer::GetIOCP(BGTestSocket::s_nNetworkId);
 
@@ -41,23 +42,31 @@ int main()
 	
 		
 	BGIOTimerThread::Init();
+		
+	BG_LOG_INFO("SERVER ON COMPLETED");
 
-	BG_LOG_INFO("Server Start");
+	//Sleep(1000);
 
 	BGTestTimerObject* testTimerObj = new BGTestTimerObject{};
 	BGIOTimerThread::AddTimer(testTimerObj, 1000, 0);
-	BGIOTimerThread::AddTimer(testTimerObj, 5000, 1);
-	BGIOTimerThread::AddTimer(testTimerObj, 10000, 2);
+	//BGIOTimerThread::AddTimer(testTimerObj, 5000, 1);
+	//BGIOTimerThread::AddTimer(testTimerObj, 10000, 2);
 
 
 
-
-	for (; ;)
+	for (int i=0; i<2; i++)
 	{
 		Sleep(1000);
 	}
 
-	BG_LOG_INFO("Server Stop");
+
+	BG_LOG_INFO("SHUTDOWN STARTED");
+	BGTestServer::Stop();
+	BGIOThread::Terminate();
+	BGIOBuffer::FreeAll();
+	BGLayer::Clear();
+	BGMainConfig::Close();
+	BG_LOG_INFO("SHUTDOWN COMPLETED");
 	
 	g_LogManager.Stop();
 
