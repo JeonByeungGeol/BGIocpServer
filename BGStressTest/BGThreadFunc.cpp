fb2a86b30f5146bff41a1d65d3215db5f88a6204
Jeon <sodtms@naver.com>
2019-01-02 16:11:26 +0900

    [1.08] StressTest : IOCP 패킷 조립 로직 구현

--- BGStressTest/BGThreadFunc.cpp
+++ BGStressTest/BGThreadFunc.cpp
@@ -5,7 +5,78 @@
 
 void WorkerThread()
 {
+	DWORD iosize;	// 이번에 받은 패킷 사이즈
+	unsigned char* buf = NULL;	// 현재 조립중인 버퍼
+	DWORD flag = 0;
+	ULONG comp_key;
+	OVERLAPPED_EX* over_ptr = NULL;
+	int remained_io_data_size = 0;
+	int result;
 
+	while (1) {
+		result = GetQueuedCompletionStatus(BGDummyClient::s_hIocp, &iosize, &comp_key, (LPOVERLAPPED*)&over_ptr, INFINITE);
+		
+		BGDummyClient* pClient = g_Client[comp_key];
+
+		if (false == result) {
+			closesocket(pClient->GetSocket());
+			std::cout << comp_key << " client 오류 발생!" << std::endl;
+			break;
+		}
+
+		if (0 == iosize) {
+			closesocket(pClient->GetSocket());
+			break;
+		}
+
+		if (IO_RECV == over_ptr->m_io_type) {
+			remained_io_data_size = iosize;
+			buf = reinterpret_cast<unsigned char*>(pClient->real_recv_buf);
+
+			while (remained_io_data_size > 0) {
+				if (0 == pClient->m_current_packet_size)
+					pClient->m_current_packet_size = buf[0];
+
+				int remained_packet_data_size = pClient->m_current_packet_size - pClient->m_uncomplete_size;
+				if (remained_packet_data_size <= remained_io_data_size) {
+					memcpy(pClient->m_uncomplete_packet + pClient->m_uncomplete_size
+						, buf
+						, remained_packet_data_size);
+					pClient->ProcessPacket(pClient->m_uncomplete_packet);
+
+					pClient->m_uncomplete_size = 0;
+					pClient->m_current_packet_size = 0;
+					buf += remained_packet_data_size;
+					remained_io_data_size += remained_packet_data_size;
+				}
+				else {
+					memcpy(pClient->m_uncomplete_packet + pClient->m_uncomplete_size, buf, remained_io_data_size);
+					pClient->m_uncomplete_size += remained_io_data_size;
+					remained_io_data_size = 0;
+				}
+			}
+
+			result = WSARecv(pClient->GetSocket(), &pClient->m_recv_wsa_buf, 1, NULL,
+				&flag, reinterpret_cast<LPWSAOVERLAPPED>(&pClient->m_overExRecv), NULL);
+
+			if (result == SOCKET_ERROR) {
+				if (WSAGetLastError() != WSA_IO_PENDING) {
+					printf("WSARecv() error!!\n");
+					return;
+				}
+			}
+
+		}
+		else if (IO_SEND == over_ptr->m_io_type) {
+			delete over_ptr;
+		}
+		else {
+			std::cout << "IO type error" << std::endl;
+			break;
+		}		
+	}
+
+	return;
 }
 
 void ConnectThread()
