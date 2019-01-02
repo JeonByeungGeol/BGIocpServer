eda5f36b8de57e33036b796be2740e2033bcb4ac
Jeon <sodtms@naver.com>
2019-01-02 14:46:05 +0900

    [1.08] 프로토콜 : ping 패킷 정의, 패킹 종류를 패킷 구조체 만들 때, 바로 지정하도록 개선

--- BGProtocal.h
+++ BGProtocal.h
@@ -1,18 +1,32 @@
 #pragma once
 
 
-/** 패킷 크기 최대 256*/
+/** 패킷 크기 최대 256 */
 using PacketSize = unsigned char;
 
-/** 패킷 종류 최대 256개 까지 정의 가능*/
-using PacketType = unsigned char;
+/** 패킷 종류 최대 ~개 까지 정의 가능 */
+enum class PacketType : unsigned short
+{
+	SC_PingTest,
+	CS_PingTest,
+};
+
+
 
 
 #pragma pack (push, 1)
-
-struct pint_test {
+struct sc_packet_ping_test {
 	PacketSize size;
-	PacketType type;
+	PacketType type{ PacketType::SC_PingTest };
 };
+
+struct cs_packet_ping_test {
+	PacketSize size;
+	PacketType type{ PacketType::CS_PingTest };
+};
+
+
+
+
 
 #pragma pack (pop)
