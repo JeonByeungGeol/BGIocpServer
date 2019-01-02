22312bb6054f0f83d4274a37107b01854865f802
Jeon <sodtms@naver.com>
2019-01-02 14:30:01 +0900

    [1.08] 프로토콜 : 패킷 구조체 패킹 사용, 패킷 크기, 패킷 종류 자료형 정의

--- BGProtocal.h
+++ BGProtocal.h
@@ -1,1 +1,18 @@
 #pragma once
+
+
+/** 패킷 크기 최대 256*/
+using PacketSize = unsigned char;
+
+/** 패킷 종류 최대 256개 까지 정의 가능*/
+using PacketType = unsigned char;
+
+
+#pragma pack (push, 1)
+
+struct pint_test {
+	PacketSize size;
+	PacketType type;
+};
+
+#pragma pack (pop)
