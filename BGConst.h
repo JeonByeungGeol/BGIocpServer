58b515f5356b3b2df9eed21a81b7fdb9dcd6de73
Jeon <sodtms@naver.com>
2019-02-08 17:16:17 +0900

    [2.02] BGTestServer에서 소켓 관리 구조 개선 : 최대 동접 가능 갯수 define으로 정의

--- BGConst.h
+++ BGConst.h
@@ -1,10 +1,10 @@
 #pragma once
 
+/** 최대 동접 수 */
+#define BG_MAX_CLIENT_NUM		10000
+
 /** 설정 파일 이름 */
 static const char* const BG_CONFIG_FILE_NAME = "bg_config.ini";
-
-/** 세션 풀 크기 : 10001개 세션 풀 유지*/
-static const int BG_SESSION_POOL_SIZE = 10001;
 
 /** IO 버퍼 크기*/
 static const int BG_BUFFER_SIZE = 4096;
